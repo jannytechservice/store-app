764ca24622c44358b0ce0a0f1b32eef0730d49d7
daniel <jannytechservice002@gmail.com>
2024-12-08 21:54:12 +0100

    fix: update MainLoader

--- src/common/loader/MainLoader.tsx
+++ src/common/loader/MainLoader.tsx
@@ -1,7 +1,27 @@
-import { CircularProgress } from '@mui/material';
+import Box, { BoxProps } from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 
-const MainLoader = () => {
-  return <CircularProgress sx={{ display: 'block', margin: '20px auto' }} />;
-};
+// ----------------------------------------------------------------------
 
-export default MainLoader;
+export default function MainLoader({ sx, ...other }: BoxProps) {
+  return (
+    <Box
+      sx={{
+        right: 0,
+        width: 1,
+        bottom: 0,
+        height: 1,
+        zIndex: 9998,
+        display: 'flex',
+        position: 'absolute',
+        alignItems: 'center',
+        justifyContent: 'center',
+        bgcolor: 'background.default',
+        ...sx,
+      }}
+      {...other}
+    >
+      <LinearProgress color="inherit" sx={{ width: 1, maxWidth: 360 }} />
+    </Box>
+  );
+}
